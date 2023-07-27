c87dd63170c587696261168c7e478afcbcda48d7
Doodle6660 <34979469+Doodle6660@users.noreply.github.com>
2023-07-27 20:39:22 +0700

    origin

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./src/styles.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+    <a href="./pages/pages.php">pages</a>
+</body>
+
+</html>
