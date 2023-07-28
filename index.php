c665ad1c8196f0461f2be96078bbf67690d34288
Doodle6660 <34979469+Doodle6660@users.noreply.github.com>
2023-07-28 15:00:42 +0700

    fixed grid using flex

--- index.php
+++ index.php
@@ -16,49 +16,54 @@ require 'functions.php';
 </head>
 
 <body class="bg-zinc-800">
-    <div class="sidebar fixed top-0 bottom-0 lg:left-0 m-7 p-2 w-[250px] overflow-y-auto text-center bg-gradient-to-b from-fuchsia-400 to-purple-800 rounded-2xl">
-        <div class="text-white text-xl">
-            <div class="p-2.5 mt-1 flex items-center">
-                <i><img class="w-14 h-14" src="https://img.icons8.com/ios/100/ffffff/happy-cloud.png" alt="happy-cloud" /></i>
-                <h1 class="font-bold text-white text-2xl ml-3">MIKROBILL</h1>
+    <div class="flex flex-row p-6 w-full">
+
+        <div class="basis-2/12">
+            <div class="sidebar w-[250px] p-2 overflow-y-auto text-center bg-gradient-to-b from-fuchsia-400 to-purple-800 rounded-2xl">
+                <div class="text-white text-xl">
+                    <div class="p-2.5 mt-1 flex items-center">
+                        <i><img class="w-14 h-14" src="https://img.icons8.com/ios/100/ffffff/happy-cloud.png" alt="happy-cloud" /></i>
+                        <h1 class="font-bold text-white text-2xl ml-3">MIKROBILL</h1>
+                    </div>
+                    <div class="my-2 bg-gray-600 h-[1px]"></div>
+                </div>
+                <div class="p-2.5 mt-10 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-fuchsia-700 text-white">
+                    <i><img class="w-5 h-5" src="https://img.icons8.com/material/48/ffffff/speedometer.png" alt="speedometer" /></i>
+                    <span class="text-[15px] ml-4 text-white font-normal">Overview</span>
+                </div>
+                <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-fuchsia-700 text-white">
+                    <i><img class="w-5 h-5" src="https://img.icons8.com/ios-filled/50/ffffff/conference-background-selected.png" alt="conference-background-selected" /></i>
+                    <span class="text-[15px] ml-4 text-white font-normal">User</span>
+                </div>
+                <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-fuchsia-700 text-white">
+                    <i><img class="w-5 h-5" src="https://img.icons8.com/ios-filled/50/ffffff/online-package-tracking.png" alt="online-package-tracking" /></i>
+                    <span class=" text-[15px] ml-4 text-white font-normal">Package</span>
+                </div>
+                <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-fuchsia-700 text-white">
+                    <i><img class="w-5 h-5" src="https://img.icons8.com/ios-filled/50/ffffff/line-chart.png" alt="line-chart" /></i>
+                    <span class="text-[15px] ml-4 text-white font-normal">Transaction!</span>
+                </div>
+                <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-fuchsia-700 text-white">
+                    <i><img class="w-5 h-5" src="https://img.icons8.com/ios-filled/50/ffffff/user-male-circle.png" alt="user-male-circle" /></i>
+                    <span class="text-[15px] ml-4 text-white font-normal">Account</span>
+                </div>
+
+                <div class="p-2.5 mt-10 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-zinc-800 hover:bg-zinc-950  text-white">
+                    <span class="text-[15px] mx-auto text-white font-bold">Logout</span>
+                </div>
             </div>
-            <div class="my-2 bg-gray-600 h-[1px]"></div>
-        </div>
-        <div class="p-2.5 mt-10 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-fuchsia-700 text-white">
-            <i><img class="w-5 h-5" src="https://img.icons8.com/material/48/ffffff/speedometer.png" alt="speedometer" /></i>
-            <span class="text-[15px] ml-4 text-white font-normal">Overview</span>
-        </div>
-        <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-fuchsia-700 text-white">
-            <i><img class="w-5 h-5" src="https://img.icons8.com/ios-filled/50/ffffff/conference-background-selected.png" alt="conference-background-selected" /></i>
-            <span class="text-[15px] ml-4 text-white font-normal">User</span>
-        </div>
-        <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-fuchsia-700 text-white">
-            <i><img class="w-5 h-5" src="https://img.icons8.com/ios-filled/50/ffffff/online-package-tracking.png" alt="online-package-tracking" /></i>
-            <span class=" text-[15px] ml-4 text-white font-normal">Package</span>
-        </div>
-        <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-fuchsia-700 text-white">
-            <i><img class="w-5 h-5" src="https://img.icons8.com/ios-filled/50/ffffff/line-chart.png" alt="line-chart" /></i>
-            <span class="text-[15px] ml-4 text-white font-normal">Transaction!</span>
-        </div>
-        <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-fuchsia-700 text-white">
-            <i><img class="w-5 h-5" src="https://img.icons8.com/ios-filled/50/ffffff/user-male-circle.png" alt="user-male-circle" /></i>
-            <span class="text-[15px] ml-4 text-white font-normal">Account</span>
         </div>
 
-        <div class="p-2.5 mt-10 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-zinc-800 hover:bg-zinc-950  text-white">
-            <span class="text-[15px] mx-auto text-white font-bold">Logout</span>
-        </div>
-    </div>
-
-    <div class="topbar w-6/12 h-14 mx-auto relative">
-        <div class="w-full h-14 top-[31px] absolute">
-            <div class="w-full h-14 left-0 top-0 flex justify-between items-center bg-neutral-600 rounded-xl px-6">
+        <div class="topbar h-14 basis-10/12 px-16 2xl:px-24">
+            <div class="h-14 left-0 top-0 flex justify-between items-center bg-neutral-600 rounded-xl px-6">
                 <div class="text-white text-base font-normal">date</div>
                 <div class="text-white text-base font-normal">admin</div>
                 <div class="text-white text-base font-normal">time</div>
             </div>
         </div>
+
     </div>
+
 
 </body>
 
